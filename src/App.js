7a70bd689859cac18620d20ed773d958537bda55
Mamta Rajput <rajputmamtabhati@gmail.com>
2022-02-02 18:22:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
+
+import React from "react";
+import "./App.css";
+import { Component } from "react";
+import Information from './emojiList.json';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+
+
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      search: null,
+
+    };
+    
+    
+  }
+  searchSpace = (event) => {
+    let keyword =event.target.value;
+    this.setState({ search: keyword })
+  }
+  render() {
+    
+    const styleInfo = {
+      paddingRight: '50px'
+    }
+    const elementStyle = {
+      border: '5px solid black',
+      borderRadius: '10px',
+      position: 'relative',
+      left: '2vh',
+      height: '4vh',
+      width: '100vh',
+      marginBottom: '2vh'
+    }
+
+
+
+
+
+    const items = Information.filter((data) => {
+      if (this.state.search == null)
+        return data
+      else if (data.title.toLowerCase().includes(this.state.search.toLowerCase()) || data.symbol.toLowerCase().includes(this.state.search.toLowerCase())) {
+        return data 
+      }
+    }).map(data => {
+      return (
+          <div class="main_div">
+            <ul>
+              <div>
+                <span style={styleInfo}>{data.symbol}{data.title}</span>
+              </div>
+            </ul>
+          </div>
+      )
+    })
+
+    return (
+      
+        <div class="i_div ">
+
+         
+        
+          <center><h1>🤗️Emoji Search🤗️</h1>
+
+          <input type="text" placeholder="Search Here..." style={elementStyle} onChange={(e) => this.searchSpace(e)} />
+          </center>
+          {items}
+          </div>
+      
+    )
+  }
+}
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
